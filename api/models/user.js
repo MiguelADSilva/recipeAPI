50dc087c9aa94dba35c8de8a39d62a122dc31537
MiguelADSilva <miguelaozinho9910@gmail.com>
2021-04-12 23:08:35 +0100

    upload images

--- api/models/user.js
+++ api/models/user.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: { 
-        type: String, 
-        require: true, 
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
-    },
-    password: { type: String, require: true },
-    firstName: { type: String, require: true },
-    lastName: { type: String, require: true },
-    imageURL: { type: String, require: false },
-    _createdAt: Date
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: { type: String, require: true },
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  userImage: { type: String, require: false },
+  _createdAt: Date,
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,95 +1,141 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.post('/signup', (req, res, next) => {
-    User
-        .find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if(user.length >= 1) {
-                res.status(409).json({ message: 'Mail Exists' })
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if(err) {
-                        res.status(500).json({ error: err })
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash,
-                            firstName: req.body.firstName,
-                            lastName: req.body.lastName,
-                            _createdAt: new Date()
-                        });
-                        user
-                            .save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({ message: 'User created'});
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({ error: err });
-                            });
-                    }
-                }); 
-            }
-        });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
 });
 
-router.post('/signin', (req, res, next) => {
-    User
-        .find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if(user.length < 1) {
-                return res.status(401).json({ message: `Auth failed` });
-            }
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                if(err) {
-                    return res.status(401).json({ message: `Auth failed` });
-                }
-                if(result) {
-                    const token = jwt.sign({
-                        email: user[0].email,
-                        userId: user[0]._id
-                    }, 
-                    process.env.JWT_KEY, 
-                    {
-                        expiresIn: '1h'
-                    }
-                    );
-                    return res.status(200).json({ 
-                        message: 'Auth successful',
-                        token: token
-                    });
-                }
-                res.status(401).json({ message: `Auth failed` });
-            })
-        })
-        .catch(err => {
-            console.log(err);
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
+});
+
+router.post("/signup", upload.single("userImage"), (req, res, next) => {
+  console.log(req.file);
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        res.status(409).json({ message: "Mail Exists" });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
             res.status(500).json({ error: err });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              userImage: req.file.path,
+              _createdAt: new Date(),
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({ message: "User created" });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({ error: err });
+              });
+          }
         });
+      }
+    });
 });
 
-router.delete('/:userId', (req, res, next) => {
-    User
-        .remove({ _id: req.params.userId })
-        .exec()
-        .then( result => {
-            console.log(result);
-            res.status(200).json({ message: 'User Deleted' })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err})
-        });
-})
+router.get("/users", (req, res, next) => {
+  User.find()
+    .select()
+    .exec()
+    .then((user) => {
+      const resp = {
+        count: user.length,
+        users: user.map((responses) => {
+          return {
+            firstName: responses.firstName,
+            lastName: responses.lastName,
+            email: responses.email,
+            password: responses.password,
+            userImage: responses.userImage,
+            _id: responses._id,
+          };
+        }),
+      };
+      res.status(200).json(resp);
+    });
+});
+
+router.post("/signin", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({ message: `Auth failed` });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({ message: `Auth failed` });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({ message: `Auth failed` });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "User Deleted" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
 
 module.exports = router;
